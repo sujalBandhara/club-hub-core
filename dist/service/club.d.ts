f7ef7b3332494fe8ff6112814643fd66bc1e6f61
sgruse <sgruse89@gmail.com>
2019-03-08 22:27:32 -0800

    Delted dist

--- dist/service/club.d.ts
+++
@@ -1,7 +0,0 @@
-import ClubHubClient from '../client';
-import Club from 'src/models/club';
-export default class ClubService {
-    client: ClubHubClient;
-    constructor(client: ClubHubClient);
-    getClubs: () => Promise<Club.UnprotectedModel[]>;
-}
