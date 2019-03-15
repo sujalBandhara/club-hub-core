2d574df61bc027d259dc764aadb309c90446f51c
John Daly <john.daly2@gmail.com>
2019-03-15 14:07:39 -0700

    Adding an HoursOfOperation type for the ReservationSettings

--- dist/models/shared.d.ts
+++ dist/models/shared.d.ts
@@ -32,5 +32,14 @@ declare namespace IShared {
         Scheduled = "SCHEDULED",
         Delivered = "DELIVERED"
     }
+    enum DayOfWeek {
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6
+    }
 }
 export default IShared;
