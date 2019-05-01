d878f9e44b9bb890955967f0b7447bd9dce01fe8
John Daly <john.daly2@gmail.com>
2019-05-01 10:53:39 -0700

    Replacing imageURL with image on Post

--- dist/models/post.d.ts
+++ dist/models/post.d.ts
@@ -1,13 +1,14 @@
 import { Types } from 'mongoose';
 import User from './user';
 import RichContent from './subModels/shared/richContent';
+import Image from './subModels/shared/image';
 declare namespace Post {
     interface Model {
         _id?: Types.ObjectId;
         clubID?: Types.ObjectId;
         title?: string;
         author?: Types.ObjectId | User.Model;
-        imageURL?: string;
+        image?: Image.Model;
         attachments?: Attachment[];
         publicationDate?: Date;
         subtitle?: string;
