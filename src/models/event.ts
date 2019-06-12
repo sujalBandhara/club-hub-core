b335ad7e2788a0fa9862de7c71c08d204bfa8b42
Kevin Coleman <kcoleman731@gmail.com>
2019-06-12 16:16:44 -0700

    Adding back fields

--- src/models/event.ts
+++ src/models/event.ts
@@ -30,6 +30,8 @@ namespace Event {
 		rDate?: string[]
 		categories?: string[]
 		location?: Location.Model
+		groupID?: Types.ObjectId[]
+		calendarID?: Types.ObjectId[]
 		calendarIDs?: Types.ObjectId[]
 		images?: Image.Model[]
 		image?: Image.Model
